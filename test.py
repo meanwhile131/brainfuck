c5447aa385be765964d5f15994182762d3e45995
meanwhile131 <51206659+meanwhile131@users.noreply.github.com>
2026-01-22 19:36:30 +0400

    chore: rename interpreter binary and make it installable

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ parser.add_argument("input")
 parser.add_argument("expected_output")
 args = parser.parse_args()
 
-process = subprocess.run(["./interpreter", args.source_file], input=args.input.encode(), stdout=subprocess.PIPE)
+process = subprocess.run(["./bf-interpreter", args.source_file], input=args.input.encode(), stdout=subprocess.PIPE)
 encoded_output = args.expected_output.encode()
 if process.stdout == encoded_output:
     exit(0)
