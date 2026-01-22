c9314e525e41a2be18d9ad8666e251e0a65e9023
meanwhile131 <51206659+meanwhile131@users.noreply.github.com>
2026-01-22 17:48:29 +0400

    test: add tests from brainfuck.org

---
+++ test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+import subprocess
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("source_file")
+parser.add_argument("input")
+parser.add_argument("expected_output")
+args = parser.parse_args()
+
+process = subprocess.run(["./interpreter", args.source_file], input=args.input.encode(), stdout=subprocess.PIPE)
+encoded_output = args.expected_output.encode()
+if process.stdout == encoded_output:
+    exit(0)
+print(f"Expected {encoded_output}, got {process.stdout}")
+exit(1)
